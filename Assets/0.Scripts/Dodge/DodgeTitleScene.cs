71dacc6356cfee9e1e964be76dcf0c1572677d2c
정찬우 <tkaghkstls2@naver.com>
2024-12-15 21:58:30 +0900

    [Add] 타이틀씬 이미지 및 코드 작업

---
+++ Assets/0.Scripts/Dodge/DodgeTitleScene.cs
@@ -0,0 +1,29 @@
+﻿using _0.Scripts.Utility;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _0.Scripts.Dodge
+{
+    public class DodgeTitleScene : MonoBehaviour
+    {
+        private bool _isLoading = false;
+
+        private AsyncOperation _sceneLoadAsync = null;
+        private void Start()
+        {
+            _sceneLoadAsync = SceneManager.LoadSceneAsync("2.MainScene");
+            _sceneLoadAsync.allowSceneActivation = false;
+            SoundManager.Instance.PlayBGM("TitleBgm");
+        }
+
+        public void Update()
+        {
+            if (_isLoading) return;
+            if (!Input.anyKeyDown) return;
+
+            if (!(_sceneLoadAsync.progress >= 0.9f)) return;
+            _isLoading = true;
+            _sceneLoadAsync.allowSceneActivation = true;
+        }
+    }
+}
